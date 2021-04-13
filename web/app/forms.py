f9727b17b77a1ebafee118fc9ae24950e2d38698
billmanh <william.jeffrey.harding@gmail.com>
2021-04-13 09:15:36 -0700

    added sliders and slider logic

--- web/app/forms.py
+++ web/app/forms.py
@@ -21,7 +21,10 @@ class HomeSystemForm(forms.Form):
     num_planets = forms.IntegerField(max_value=10,min_value=1,initial=6,label='Number of planets',widget=NumberInput,help_text='(not planet-like objects)')
     num_moons = forms.IntegerField(max_value=10,min_value=1,initial=24,label='Number of moons',widget=NumberInput,help_text='(randomly distributed to planets)')
     home_has_moons = forms.BooleanField(initial=True,help_text='ensure that your planet has at least one moon')
-    #TODO : Add separater for fields somehow
-    # sep = forms.CharField(label='sep')
-
+    starting_pop = forms.IntegerField(max_value=20,min_value=1,initial=7,label='Homeworld Population',widget=NumberInput,help_text='(population is in millions of people)')
+    population_conformity = forms.FloatField(max_value=1,min_value=0,initial=.3,label='Population Conformity',widget=NumberInput,help_text='( % How much your people consider themselves "one people")')
+    population_literacy = forms.FloatField(max_value=1,min_value=0,initial=.7,label='Homeworld Literacy',widget=NumberInput,help_text='( % affects the tech advancement)')
+    population_aggression = forms.FloatField(max_value=1,min_value=0,initial=.5,label='Population Aggression',widget=NumberInput,help_text='( % agressive to "others")')
+    population_constitution = forms.FloatField(max_value=1,min_value=0,initial=.5,label='Population Aggression',widget=NumberInput,help_text='( %  ability to persivere)')
+    
 
