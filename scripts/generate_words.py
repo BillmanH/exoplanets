155b56b8aa42e728593a913ba405441083762a08
billmanh <william.jeffrey.harding@gmail.com>
2021-04-24 06:59:28 -0700

    fixed issue with words

--- scripts/generate_words.py
+++ scripts/generate_words.py
@@ -1,7 +1,12 @@
 # %%
 
-import pandas as pd
+import pickle
+import re
+
 import numpy as np
+import pandas as pd
+from nltk import word_tokenize
+from nltk.tokenize import SyllableTokenizer
 
 # %%
 cities = pd.read_csv("../data/world-cities.csv")
@@ -16,10 +21,6 @@ co = cities["country"].drop_duplicates().str.lower().values
 words = pd.concat([pd.DataFrame(ci), pd.DataFrame(co)]).drop_duplicates().values
 words = " ".join(words.flatten())
 
-from nltk import word_tokenize
-
-# %%
-from nltk.tokenize import SyllableTokenizer
 
 SSP = SyllableTokenizer()
 
@@ -38,14 +39,18 @@ pd.DataFrame(syllables).to_csv("../data/syllables.csv", encoding="utf-8")
 pickle.dump(syllables, open("../data/syllables.p", "wb"))
 syllables = pickle.load(open("../data/syllables.p", "rb"))
 
+
+syl = [i for i in syllables if type(i)==str]
+clean_syll = [i for i in syl if re.match("[a-z]+$",i)]
+len(syllables),len(syl),len(clean_syll)
+
+pickle.dump(clean_syll, open("../data/syllables.p", "wb"))
 #%%
 def make_word(n):
-    syl = np.random.choice(syllables, n)
+    syl = np.random.choice(clean_syll, n)
     word = "".join(syl)
     return word
 
 
-make_word(3)
+[make_word(3) for word in range(10)]
 
-# %%
-import pickle
