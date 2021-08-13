763fe0c6470527af8069f2c8dfb44af2248a83e4
billmanh <william.jeffrey.harding@gmail.com>
2021-08-13 07:45:20 -0700

    Added second screen for planets

--- web/app/templates/app/charts/planet_system.js
+++ web/app/templates/app/charts/planet_system.js
@@ -4,59 +4,62 @@ var width = 640,
     defaultNodeSize = 10,
     orbitalStrength = .15;
 
-var svg = d3.select('body').append('svg')
+var plan_svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height)
     .classed('map', true)
-    .classed('system', true)
-    .attr("id", 'map_system');
+    .classed('planet', true)
+    .attr("id", 'map_planet');
 
-s_b_box = document.querySelector('#map_system').getBoundingClientRect()
+p_b_box = document.querySelector('#map_planet').getBoundingClientRect()
 
-var s_objectColors = {
+
+var p_objectColors = {
     'G': '#FDB813',
-    'moon':'#F4F1C9',
-    'terrestrial':'#3644E4',
-    'ice':'#A7DEDA',
-    'dwarf':'#0EC0A6'
+    'moon': '#F4F1C9',
+    'terrestrial': '#3644E4',
+    'ice': '#A7DEDA',
+    'dwarf': '#0EC0A6'
 }
 // normalize the radius
 
-s_planetScale = d3.scaleLog()
+p_planetScale = d3.scaleLog()
     .domain(
         [
             d3.min(nodes, function (d) { return d.radius; }),
             d3.max(nodes, function (d) { return d.radius; })]
     ).range([0, 20]);
 
-s_orbitScale = d3.scaleLinear()
+p_orbitScale = d3.scaleLinear()
     .domain(
         [
             d3.min(nodes, function (d) { return d.orbitsDistance; }),
             d3.max(nodes, function (d) { return d.orbitsDistance; })]
     ).range([0, 20]);
 
-var s_force = d3.forceSimulation(nodes)
+var planet_force = d3.forceSimulation(nodes)
     .force('charge', d3.forceManyBody())
     .force("link", d3.forceLink(links)
         .id(d => d.id)
-        .distance(function(d) {return s_orbitScale(d['source'].orbitsDistance)})
-        .strength(orbitalStrength)
-    ) 
-    .force('center', d3.forceCenter(width / 2, height / 2))
-    .on('tick', s_ticked);
+        .distance(function (d) { return p_orbitScale(d['source'].orbitsDistance) })
+        .strength(w)
+    )
+    .force('center', d3.forceCenter(p_b_box.right - (p_b_box.width / 2),
+        p_b_box.bottom - (p_b_box.height / 2))
+    )
+    .on('tick', planet_ticked);
 
 
-var s_point = {}
-function s_ticked() {
-    var u = d3.select('#map_system')
+var point = {}
+function planet_ticked() {
+    var u = d3.select('#map_planet')
         .selectAll('circle')
         .data(nodes)
 
     u.enter()
         .append('circle')
-        .attr('r', function (d) { return s_planetScale(d.radius) })
-        .style("fill", function (d) { return s_objectColors[d.class] })
+        .attr('r', function (d) { return p_planetScale(d.radius) })
+        .style("fill", function (d) { return p_objectColors[d.class] })
         .attr("stroke", 'black')
         .merge(u)
         .attr('cx', function (d) {
