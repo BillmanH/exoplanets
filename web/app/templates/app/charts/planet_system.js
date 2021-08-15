09a878f15f269c159971efa149aa5628fd3dd5a6
billmanh <william.jeffrey.harding@gmail.com>
2021-08-15 14:13:51 -0700

    Template for nodemap, way better

--- web/app/templates/app/charts/planet_system.js
+++ web/app/templates/app/charts/planet_system.js
@@ -1,82 +1,19 @@
 
-
-var s_orbitalStrength = .15;
-
-var plan_svg = d3.select('body').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .classed('map', true)
-    .classed('planet', true)
-    .attr("id", 'map_planet');
-
-p_b_box = document.querySelector('#map_planet').getBoundingClientRect()
-
-
-var p_objectColors = {
+var s_objectColors = {
     'G': '#FDB813',
-    'moon': '#F4F1C9',
-    'terrestrial': '#3644E4',
-    'ice': '#A7DEDA',
-    'dwarf': '#0EC0A6'
+    'moon':'#F4F1C9',
+    'terrestrial':'#3644E4',
+    'ice':'#A7DEDA',
+    'dwarf':'#0EC0A6'
 }
-// normalize the radius
 
-p_planetScale = d3.scaleLog()
-    .domain(
-        [
-            d3.min(nodes, function (d) { return d.radius; }),
-            d3.max(nodes, function (d) { return d.radius; })]
-    ).range([0, 20]);
-
-p_orbitScale = d3.scaleLinear()
-    .domain(
-        [
-            d3.min(nodes, function (d) { return d.orbitsDistance; }),
-            d3.max(nodes, function (d) { return d.orbitsDistance; })]
-    ).range([0, 20]);
-
-var planet_force = d3.forceSimulation(nodes)
-    .force('charge', d3.forceManyBody())
-    .force("link", d3.forceLink(links)
-        .id(d => d.id)
-        .distance(function (d) { return p_orbitScale(d['source'].orbitsDistance) })
-        .strength(s_orbitalStrength)
-    )
-    .force('center', d3.forceCenter(p_b_box.right - (p_b_box.width / 2),
-        p_b_box.bottom - (p_b_box.height / 2))
-    )
-    .on('tick', planet_ticked);
-
-
-var point = {}
-function planet_ticked() {
-    var u = d3.select('#map_planet')
-        .selectAll('circle')
-        .data(nodes)
-
-    u.enter()
-        .append('circle')
-        .attr('r', function (d) { return p_planetScale(d.radius) })
-        .style("fill", function (d) { return p_objectColors[d.class] })
-        .attr("stroke", 'black')
-        .merge(u)
-        .attr('cx', function (d) {
-            return d.x
-        })
-        .attr('cy', function (d) {
-            return d.y
-        })
-        .on("mouseover", (event, d) => {
-            return tooltip.style("visibility", "visible").html(dictToHtml(d));
-        })
-        .on("mousemove", (event) => {
-            return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px")
-        })
-        .on("mouseout", (event) => {
-            d3.pointer(event)
-            return tooltip.style("visibility", "hidden");
-        })
-
-    u.exit().remove()
-}
+dwaw_node(
+    "pSystem",
+    nodes,
+    links,
+    s_objectColors,
+    .15,
+    height,
+    width
+)
 
--- web/app/templates/app/charts/solar_system.js
+++ web/app/templates/app/charts/solar_system.js
@@ -1,16 +1,4 @@
 
-
-var p_orbitalStrength = .15;
-
-var svg = d3.select('body').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .classed('map', true)
-    .classed('system', true)
-    .attr("id", 'map_system');
-
-s_b_box = document.querySelector('#map_system').getBoundingClientRect()
-
 var s_objectColors = {
     'G': '#FDB813',
     'moon':'#F4F1C9',
@@ -18,62 +6,14 @@ var s_objectColors = {
     'ice':'#A7DEDA',
     'dwarf':'#0EC0A6'
 }
-// normalize the radius
 
-s_planetScale = d3.scaleLog()
-    .domain(
-        [
-            d3.min(nodes, function (d) { return d.radius; }),
-            d3.max(nodes, function (d) { return d.radius; })]
-    ).range([0, 20]);
-
-s_orbitScale = d3.scaleLinear()
-    .domain(
-        [
-            d3.min(nodes, function (d) { return d.orbitsDistance; }),
-            d3.max(nodes, function (d) { return d.orbitsDistance; })]
-    ).range([0, 20]);
-
-var s_force = d3.forceSimulation(nodes)
-    .force('charge', d3.forceManyBody())
-    .force("link", d3.forceLink(links)
-        .id(d => d.id)
-        .distance(function(d) {return s_orbitScale(d['source'].orbitsDistance)})
-        .strength(p_orbitalStrength)
-    ) 
-    .force('center', d3.forceCenter(width / 2, height / 2))
-    .on('tick', s_ticked);
-
-
-var s_point = {}
-function s_ticked() {
-    var u = d3.select('#map_system')
-        .selectAll('circle')
-        .data(nodes)
-
-    u.enter()
-        .append('circle')
-        .attr('r', function (d) { return s_planetScale(d.radius) })
-        .style("fill", function (d) { return s_objectColors[d.class] })
-        .attr("stroke", 'black')
-        .merge(u)
-        .attr('cx', function (d) {
-            return d.x
-        })
-        .attr('cy', function (d) {
-            return d.y
-        })
-        .on("mouseover", (event, d) => {
-            return tooltip.style("visibility", "visible").html(dictToHtml(d));
-        })
-        .on("mousemove", (event) => {
-            return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px")
-        })
-        .on("mouseout", (event) => {
-            d3.pointer(event)
-            return tooltip.style("visibility", "hidden");
-        })
-
-    u.exit().remove()
-}
+dwaw_node(
+    "sSystem",
+    nodes,
+    links,
+    s_objectColors,
+    .15,
+    height,
+    width
+)
 
