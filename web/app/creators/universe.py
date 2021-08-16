32cb673532cd5b1769864c416646bfde648e7fbc
billmanh <william.jeffrey.harding@gmail.com>
2021-08-16 13:47:42 -0700

    Added homeworld function

--- web/app/creators/universe.py
+++ web/app/creators/universe.py
@@ -9,34 +9,18 @@ print(os.listdir())
 # Depending on where this is run, it could be back one dir.
 try:
     syllables = pickle.load(open("../data/syllables.p", "rb"))
-    pdata = yaml.safe_load(open('creators/specs/planet.yaml'))
+    pdata = yaml.safe_load(open('web/app/creators/specs/planet.yaml'))["planet_types"]
 except FileNotFoundError:
     syllables = pickle.load(open("data/syllables.p", "rb"))
     pdata = yaml.safe_load(open('web/app/creators/specs/planet.yaml'))["planet_types"]
+    mdata = yaml.safe_load(open('web/app/creators/specs/moon.yaml'))["moon_types"]
 
 # TODO Get some stats on star types
 # sdata = {"radius_mean": 109, "radius_std": 1, "class": "G"}
 sdata = {"radius": 106, "class": "G"}
 
 
-mdata = {
-    "moon": {
-        "count": 13.0,
-        "prob": 0.8125,
-        "mass_mean": 0.005513153846153847,
-        "mass_std": 0.009417033882333949,
-        "radius_mean": 0.17966153846153846,
-        "radius_std": 0.12918538706358867,
-    },
-    "terrestrial": {
-        "count": 3.0,
-        "prob": 0.1875,
-        "mass_mean": 0.011778333333333333,
-        "mass_std": 0.0035116816959020257,
-        "radius_mean": 0.26786666666666664,
-        "radius_std": 0.020873987001369262,
-    },
-}
+
 
 #%%
 def make_word(n, spaces=True):
@@ -83,11 +67,19 @@ def make_planet(t, orbiting):
     planet["objid"] = uuid(n=13)
     planet["mass"] = abs(r.normal(pdata[t]["mass_mean"], pdata[t]["mass_std"]))
     planet["radius"] = abs(r.normal(pdata[t]["radius_mean"], pdata[t]["radius_std"]))
-    planet["orbitsDistance"] = sort_planets(t)
+    planet["orbitsDistance"] = rnd(pdata[t]["distance_min"], pdata[t]["distance_max"])
     planet["orbitsId"] = orbiting["objid"]
     planet["orbitsName"] = orbiting["name"]
+    planet["isSupportsLife"] = False
+    planet["isPopulated"] = False
     return planet
 
+def make_homeworld(orbiting, data):
+    planet = make_planet("terrestrial", orbiting)
+    planet["name"] = data["planet_name"]
+    planet["isSupportsLife"] = True
+    planet["isPopulated"] = True
+    return planet
 
 def make_moon(t, planets):
     moon = {"class": t, "name": make_word(rnd(2, 1))}
@@ -99,6 +91,7 @@ def make_moon(t, planets):
     moon["orbitsId"] = orbiting["objid"]
     moon["orbitsDistance"] = .005
     moon["orbitsName"] = orbiting["name"]
+    moon["isSupportsLife"] = False
     return moon
 
 
@@ -121,8 +114,9 @@ def build_homeSystem(data, username):
             r.choice(list(pdata.keys()), p=[pdata[t]["prob"] for t in pdata.keys()]),
             star,
         )
-        for p in range(int(data["num_planets"]))
+        for p in range(int(data["num_planets"])-1)
     ]
+    homeworld = make_homeworld(star, data)
     moons = [
         make_moon(
             r.choice(list(mdata.keys()), p=[mdata[t]["prob"] for t in mdata.keys()]),
@@ -130,7 +124,7 @@ def build_homeSystem(data, username):
         )
         for p in range(int(data["num_moons"]))
     ]
-    nodes = [user] + [system] + [star] + moons + planets
+    nodes = [user] + [system] + [star] + moons + planets + [homeworld]
     system_edges = [
         {"node1": p["objid"], "node2": system["objid"], "label": "isInSystem"}
         for p in nodes
