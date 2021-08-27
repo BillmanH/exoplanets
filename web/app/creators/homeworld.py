42aba1d6817f8a0b2fb2e3983f3cb700ee47640c
billmanh <william.jeffrey.harding@gmail.com>
2021-08-27 16:00:45 -0700

    started on Homeworld

--- web/app/creators/homeworld.py
+++ web/app/creators/homeworld.py
@@ -1,7 +1,11 @@
-def build_homeSystem(data, username):
-    accountid = uuid(n=13)
-    user = {
-        "label": "account",
-        "created": datetime.now().strftime("%d-%m-%Y-%H-%M-%S"),
-        "objid": accountid,
-    }
+def build_species(data):
+    # More rules here to follow depending on the attribute, however this is a placeholder
+    species = {}
+    for attr in ['population_conformity', 'population_literacy', 'population_aggression', 'population_constitution']:
+        species[attr] = data[attr]
+    return species
+
+
+def build_homeSystem(data):
+    pass
+
