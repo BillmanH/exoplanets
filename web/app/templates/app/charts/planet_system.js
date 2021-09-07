c45bd919d9bbe25a708837b400a905c3014e7b73
billmanh <william.jeffrey.harding@gmail.com>
2021-09-07 07:24:13 -0700

    new data working propperly in UI

--- web/app/templates/app/charts/planet_system.js
+++ web/app/templates/app/charts/planet_system.js
@@ -18,11 +18,11 @@ var planet_table_lables = [{"label":"Name","value":"name"},
                             {"label":"Supports Life","value":"isSupportsLife"}
                         ]
 
-var population_table_lables = [{"label":"Name","value":"name"}, 
-                            {"label":"Aggression","value":"population_aggression"},
-                            {"label":"Conformity","value":"population_conformity"},
-                            {"label":"Constitution","value":"population_constitution"},
-                            {"label":"Literacy","value":"population_literacy"}
+var table_lables = [{"label":"Name","value":"name"}, 
+                            {"label":"Aggression","value":"aggression"},
+                            {"label":"Conformity","value":"conformity"},
+                            {"label":"Constitution","value":"constitution"},
+                            {"label":"Literacy","value":"literacy"}
                         ]
                     
 var faction_table_lables = [{"label":"Faction Name","value":"name"}]
@@ -44,7 +44,7 @@ function clickTablePlanet(d){
                 draw_table(
                     "peopleTable",
                     data['pops'],
-                    population_table_lables
+                    table_lables
                     )
                 draw_table(
                     "factionTable",
