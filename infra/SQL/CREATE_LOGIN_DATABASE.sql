689a2954871e7a031d90ad44764f9a32503062b0
billmanh <william.jeffrey.harding@gmail.com>
2021-12-24 07:23:37 -0800

    time to test the new postgres

--- infra/SQL/CREATE_LOGIN_DATABASE.sql
+++ infra/SQL/CREATE_LOGIN_DATABASE.sql
@@ -1,1 +1,9 @@
-CREATE DATABASE exo
+-- create user djangologin with encrypted password 'XfRVldD9K88Hekwz';
+ -- create database exologin with encoding 'utf8' owner djangologin;
+
+SELECT *
+FROM pg_catalog.pg_user;
+
+
+select *
+from pg_catalog.pg_tables;
