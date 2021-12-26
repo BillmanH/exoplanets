6ac7b6d959414ad9af92f24e06cc3b3097054147
billmanh <william.jeffrey.harding@gmail.com>
2021-12-26 12:01:39 -0800

    Fixed issue where local vars weren't being set right

--- infra/setting_local-env_vars.ps1
+++ infra/setting_local-env_vars.ps1
@@ -2,14 +2,19 @@
 conda activate exoplanets
 # Load the json file:
 $json = Get-Content 'infra\env-vars.json' | Out-String | ConvertFrom-Json
+$omited = "ALLOWED_HOSTS", "stage", "abspath", ""
 
 foreach($v in $json.PSObject.Properties)
 {
-    if ($v.Name -eq "ALLOWED_HOSTS" -or $v.Name -eq "stage") {
+    if ($omited -contains $v.Name) {
         Write-Host $v.Name "is omitted"
     } else {
+        $valstring = $v.Value | Out-String
+        $namstring = $v.Name | Out-String
+        $command = "conda env config vars set $namstring=$valstring" 
+        $command = $command -replace "`t|`n|`r",""
         $v.Name + " : " + $v.Value
-        conda env config vars set $v.Name=$v.Value
+        Invoke-expression $command
     }
 }
 
