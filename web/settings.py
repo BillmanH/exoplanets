0abd5fb5d3fbe809eb3dfc16cf244a370c902b00
billmanh <william.jeffrey.harding@gmail.com>
2022-01-01 10:52:17 -0800

    some formatting

--- web/settings.py
+++ web/settings.py
@@ -121,13 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static and Media Files
