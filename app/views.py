7b6be4127320f52826450452a5b25a63f2cb1a27
billmanh <william.jeffrey.harding@gmail.com>
2022-02-03 07:56:44 -0800

    genisis view created to hold gen process

--- app/views.py
+++ app/views.py
@@ -71,20 +71,23 @@ def new_universe(request):
         account.drop_account(c, username)
         # Create the new system
         universe_nodes, universe_edges = universe.build_homeSystem(request.POST, username)
-        # Create the homeworld and it's people. 
-        homeworld_nodes, homeworld_edges = homeworld.build_people(request.POST)
-        # Attach the people to the homeworld
-        homeworld_edges = homeworld_edges + homeworld.attach_people_to_world(homeworld_nodes,universe_nodes)
-        # Get the global list of objectives (prerequisite of desires)
-        res = run_query(c, query="g.V().hasLabel('objective').valueMap()")
-        objectives = [clean_node(n) for n in res]
-        # Get the pop desire for those objectives
-        homeworld_edges = homeworld_edges + homeworld.get_pop_desires([p for p in homeworld_nodes if p['label']=='pop'],objectives)
         # Upload all of that data that was created. 
-        data = {"nodes": universe_nodes + homeworld_nodes, "edges": universe_edges + homeworld_edges}
+        data = {"nodes": universe_nodes, "edges": universe_edges}
         upload_data(c, username, data)
         # load the galaxy map, thus starting the game
         return redirect("genesis")
+        
+        # # Create the homeworld and it's people. 
+        # homeworld_nodes, homeworld_edges = homeworld.build_people(request.POST)
+        # # Attach the people to the homeworld
+        # homeworld_edges = homeworld_edges + homeworld.attach_people_to_world(homeworld_nodes,universe_nodes)
+        # # Get the global list of objectives (prerequisite of desires)
+        # res = run_query(c, query="g.V().hasLabel('objective').valueMap()")
+        # objectives = [clean_node(n) for n in res]
+        # # Get the pop desire for those objectives
+        # homeworld_edges = homeworld_edges + homeworld.get_pop_desires([p for p in homeworld_nodes if p['label']=='pop'],objectives)
+
+
     if request.method == "GET":
         form = HomeSystemForm()
         context["form"] = form
@@ -94,13 +97,10 @@ def new_universe(request):
 @login_required
 def genesis(request):
     c = get_client()
-    context = {}
-    form = HomeSystemForm(request.GET or None)
-    username = request.user.username
-    universe_nodes, universe_edges = universe.build_homeSystem(request.GET, username)
-    data = {"nodes": universe_nodes + homeworld_nodes, "edges": universe_edges + homeworld_edges}
-    upload_data(c, username, data)
-    return render(request, "app/creation/new_universe.html", context)
+    res = get_system(c, request.user.username)
+    context = {"solar_system": res}
+    c.close()
+    return render(request, "app/creation/genesis_view.html", context)
 
 
 @login_required
