9e28be240c9d5dfd7b1baa7c26f611faf45437ec
billmanh <william.jeffrey.harding@gmail.com>
2022-05-07 20:58:56 -0700

    build_az now has functions

--- infra/build_az.ps1
+++ infra/build_az.ps1
@@ -1,3 +1,5 @@
+# I generally run this one section at a time, not all at once. 
+
 # Script variables:
 $resourceGroupName = "exodestiny"
 $location = "West US 2"
@@ -15,3 +17,8 @@ cd $Env:abspath
 # *_parameters are in the .gitignore 
 # Single turnkey resources from template. 
 az deployment group create --resource-group $resourceGroupName --template-file "infra/ARM/template.json" --parameters "infra/ARM/parameters.json"
+
+
+# Creating the azure functions separately until I get it figured out. 
+az deployment group create --resource-group $resourceGroupName --template-file "infra/ARM/template_func.json" --parameters "infra/ARM/parameters_func.json"
+
