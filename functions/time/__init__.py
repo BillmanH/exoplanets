2ad1cdd193c944e8560b0ffb897f8a418c368a7e
billmanh <william.jeffrey.harding@gmail.com>
2022-09-03 09:28:00 -0700

    time: global ticker now separate function

--- functions/time/__init__.py
+++ functions/time/__init__.py
@@ -3,6 +3,7 @@ import logging
 
 import azure.functions as func
 from .db_functions import get_client, run_query, clean_node
+from .time_functions import global_ticker
 
 logger = logging.getLogger('azure.mgmt.resource')
 
@@ -15,18 +16,18 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
     
-    # Get the current time node
+    # Time is constant, pulled in once at the begining of the function
     res = run_query(c,"g.V().hasLabel('time').valueMap()")
     time = [clean_node(n) for n in res][0]
 
-    # Autoincrement time by one:
-    currentTime = time['currentTime'] + 1
-    logging.info(f"time was discovered at:")
-    logging.info(time)
-    # TODO: Update function to indicate from where time was updated (e.g. notebook, local function, prod function)
-    updateRes = run_query(c, f"g.V().hasLabel('time').property('currentTime', {currentTime})")
+    ### Start of time_funtions here
+    global_ticker(c,time)
 
-    logging.info(f"currentTime was updated to: {currentTime}")
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    ### End of time_functions
+
+
+
+    logging.info(f'Python timer trigger function ran at: {utc_timestamp}')
     c.close()
 
--- functions/time/time_functions.py
+++ functions/time/time_functions.py
@@ -1,4 +1,15 @@
-def processActions(c):
-    
+import logging
 
-    return resp
+from .db_functions import run_query
+
+def global_ticker(c,time):
+    """
+    Increment the time by one. Takes a graph connection `c` and the universal time node `time`.
+    """
+    # Autoincrement time by one:
+    currentTime = time['currentTime'] + 1
+    logging.info(f"time was discovered at: {time}")
+
+    
+    updateRes = run_query(c, f"g.V().hasLabel('time').property('currentTime', {currentTime}).propperty('updatedFrom','azfunction')")
+    logging.info(f"currentTime was updated to: {currentTime}")
