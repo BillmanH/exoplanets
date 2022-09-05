223b97567dd999f2361ed11cdddd5c508c1c0f18
billmanh <william.jeffrey.harding@gmail.com>
2022-09-05 14:44:27 -0700

    troubleshooting

--- functions/time/__init__.py
+++ functions/time/__init__.py
@@ -34,10 +34,10 @@ def main(mytimer: func.TimerRequest) -> None:
             if 'augments_self_properties' in a['job'].keys():
                 patch = run_query(c, augments_self_properties(a['agent'],a['job']))
             # set the job to resolved
-            run_query(c, f"g.V().has('objid','{a['agent']['objid']}').outE('takingAction').has('actionType', '{a['job']['actionType']}').has('status', 'pending').has('weight',{a['job']['weight']}).property('status', 'resolved')")
+            res = run_query(c, f"g.V().has('objid','{a['agent']['objid']}').outE('takingAction').has('actionType', '{a['job']['actionType']}').has('status', 'pending').has('weight',{a['job']['weight']}).property('status', 'resolved')")
             # set the agent to isIdle=True
             if 'isIdle' in a['agent'].keys():
-                run_query(c, f"g.V().has('objid','{a['agent']['objid']}').property('isIdle','true')")
+                res = run_query(c, f"g.V().has('objid','{a['agent']['objid']}').property('isIdle','true')")
 
     logging.info(f'Total ations resolved in this run: {validActionCounter}')
 
