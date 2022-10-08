0e9715861452bd4a754a978f9c9dfa2bdafba333
billmanh <william.jeffrey.harding@gmail.com>
2022-10-08 09:52:40 -0700

    Added tests for new actions

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -10,9 +10,22 @@ actions = load_actions()
 def test_more_than_one_action():    
     assert len(actions)>1  
 
-def action_augments_self_properties:
-    if action['augments_self_properties']:
-        
+def action_augments_self_properties(action):
+    items = action['augments_self_properties'].split(';')
+    properties = items[0].split(',')
+    mods = items[1].split(',')
+    assert len(properties)==len(mods)
+
+def action_requires_attr(action):
+    items = action['requires_attr'].split(';')
+    properties = items[0].split(',')
+    mods = items[1].split(',')
+    assert len(properties)==len(mods)
+
+
 def test_action_properties():
     for action in actions: 
-        assert
+        if "augments_self_properties" in action.keys():
+            action_augments_self_properties(action)
+        if "requires_attr" in action.keys():
+            action_requires_attr(action)
