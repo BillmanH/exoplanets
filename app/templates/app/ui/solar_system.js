47b7f60c65f590aad397e4317067db25925b703d
billmanh <william.jeffrey.harding@gmail.com>
2022-12-09 09:03:45 -0800

    Solar system is still wip but making progress

--- app/templates/app/ui/solar_system.js
+++ app/templates/app/ui/solar_system.js
@@ -1,15 +1,36 @@
 // const planet = BABYLON.SceneLoader.Append("static/app/objects/", "planet.gltf", scene, function (scene) { });
 // https://doc.babylonjs.com/typedoc/classes/BABYLON.SceneLoader#Append
 
+function get_values(l,v){
+    var mylist = []
+    for (let i = 0; i < l.length; i++){
+        if(l[i][v]!=undefined){
+            mylist.push(l[i][v])
+        }
+    } 
+    return mylist
+}
+
+var scale_radius = d3.scaleLog()
+            .domain(get_values(solar_system["nodes"],"radius"))
+            .range([1,20]);
+
+var scale_distance = d3.scaleLinear()
+            .domain(get_values(solar_system["nodes"],"orbitsDistance"))
+            .range([20,100]);
+
 function createStar(n){
-    const star = BABYLON.MeshBuilder.CreateSphere(n.name, {diameter: (n.radius+n.radius)}, scene);
-    star.scaling = new BABYLON.Vector3(n.radius,n.radius,n.radius);
+    const star = BABYLON.MeshBuilder.CreateSphere(n.name, {diameter: scale_radius(n.radius)});
+    // const material = new BABYLON.StandardMaterial("app");
+    star.ambientColor = new BABYLON.Color3(249, 180, 24);
     return star
 }
 
 function createPlanet(n){
-    const planet = BABYLON.MeshBuilder.CreateSphere(n.name, {diameter: (n.radius+n.radius)}, scene);
-    planet.position = new BABYLON.Vector3(0,n.orbitsDistance,n.orbitsDistance);
+    const planet = BABYLON.MeshBuilder.CreateSphere(n.name,  {diameter: scale_radius(n.radius)});
+    planet.position = new BABYLON.Vector3(0,0,n.orbitsDistance*2);
+
+    const torus = BABYLON.MeshBuilder.CreateTorus(n.name+"-ring", {diameter: n.orbitsDistance*2});
     return planet
 }
 
@@ -17,10 +38,10 @@ for (let i = 0; i < solar_system.nodes.length; i++) {
     n = solar_system["nodes"][i]
     if (n["objtype"]=="star"){
         createStar(n)
-        console.log("star created")
+        console.log("star: " + n.name +"; radius: "+ n.radius)
     }
     if (n["objtype"]=="planet"){
-        console.log("planet created: " + n.name)
+        console.log("planet: " + n.name +"; radius: "+ n.radius +" ("+scale_radius(n.radius)+")" + "; orbitsDistance: " + n.orbitsDistance )
         createPlanet(n)
     }
   }
