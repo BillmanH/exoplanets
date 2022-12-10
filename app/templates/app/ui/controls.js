afb196609ef8577c0dd3fed268eeb6c2ea06f3a7
billmanh <william.jeffrey.harding@gmail.com>
2022-12-10 11:46:01 -0800

    Added 3d view to menu

--- app/templates/app/ui/controls.js
+++ app/templates/app/ui/controls.js
@@ -1,23 +1,23 @@
-// https://doc.babylonjs.com/features/featuresDeepDive/gui/gui
+// // https://doc.babylonjs.com/features/featuresDeepDive/gui/gui
 
 
 
 
-// // GUI gonna come back to it. 
-var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Center on Homeworld");
+// // // GUI gonna come back to it. 
+// var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+// var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Center on Homeworld");
 
-button1.width = "150px"
-button1.height = "40px"
-button1.top = "-100"
-button1.left = "-100"
+// button1.width = "150px"
+// button1.height = "40px"
+// button1.top = "-100"
+// button1.left = "-100"
 
-button1.color = "white";
-button1.cornerRadius = 10;
-button1.background = "black";
+// button1.color = "white";
+// button1.cornerRadius = 10;
+// button1.background = "black";
 
-// button1.onPointerUpObservable.add(function() {
-//     alert("you did it!");
-// });
+// // button1.onPointerUpObservable.add(function() {
+// //     alert("you did it!");
+// // });
 
-advancedTexture.addControl(button1);
+// advancedTexture.addControl(button1);
--- app/templates/app/ui/solar_system.js
+++ app/templates/app/ui/solar_system.js
@@ -2,18 +2,42 @@
 // https://doc.babylonjs.com/typedoc/classes/BABYLON.SceneLoader#Append
 {% load static %}
 
-star_base = 10
+sphere_textures = {
+    "G":"{% static 'app/objects/planet/star_g.png' %}",
+    "terrestrial":"{% static 'app/objects/planet/terrestrial_oceanic.png' %}",
+    "dwarf":"{% static 'app/objects/planet/dwarf.png' %}",
+    "gas":"{% static 'app/objects/planet/gas_giant_blue.png' %}",
+    "ice":"{% static 'app/objects/planet/ice.png' %}",
+}
+
+star_base = 100
+shinyness = 0.05
 
 function createStar(n){
     const star = BABYLON.MeshBuilder.CreateSphere(n.name, {diameter: star_base});
-    const surface = new BABYLON.StandardMaterial("{% static 'app/objects/star.jpg' %}");
+    var sunlight = new BABYLON.PointLight("sunlight", new BABYLON.Vector3(0, 0, 0));
+    const surface = new BABYLON.StandardMaterial("surface");
+    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.class]);
+    
+    // light coming of sun
+    surface.emissiveColor = new BABYLON.Color3(0, 1, 1);
+    sunlight.diffuse = new BABYLON.Color3(0, 1, 1);
+    sunlight.specular = new BABYLON.Color3(0, 1, 1);
+
     star.material = surface
+
     return star
 }
 
 function createPlanet(n){
     const planet = BABYLON.MeshBuilder.CreateSphere(n.name,  {diameter: scale_radius(n.radius)});
     planet.position = new BABYLON.Vector3(scale_distance(Math.random()),0,scale_distance(n.orbitsDistance));
+
+    // texture
+    const surface = new BABYLON.StandardMaterial("surface");
+    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.class]);
+    planet.material = surface
+    planet.material.specularColor = new BABYLON.Color3(shinyness, shinyness, shinyness);
 
     // GUI
     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
