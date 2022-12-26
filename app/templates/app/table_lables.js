13ebfd6e95066c455dc9810a313bbbd55a9ba629
billmanh <william.jeffrey.harding@gmail.com>
2022-12-26 15:29:52 -0800

    moved table lables to it's own thing

---
+++ app/templates/app/table_lables.js
@@ -0,0 +1,13 @@
+var pop_table_lables = [{"label":"Name","value":"name"}, 
+                            {"label":"Aggression","value":"aggression"},
+                            {"label":"Conformity","value":"conformity"},
+                            {"label":"Constitution","value":"constitution"},
+                            {"label":"Literacy","value":"literacy"},
+                            {"label":"Wealth","value":"wealth"},
+                            {"label":"Health","value":"health"}
+                        ]
+
+var faction_table_lables = [
+    { "label": "Name", "value": "name" },
+    { "label": "Type", "value": "label" }
+]
