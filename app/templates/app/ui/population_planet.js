0ca61506cc68b212e6a7096e3138c6d66dd67789
billmanh <william.jeffrey.harding@gmail.com>
2023-01-14 12:07:15 -0800

    reorganized the data object

--- app/templates/app/ui/population_planet.js
+++ app/templates/app/ui/population_planet.js
@@ -1,5 +1,18 @@
 {% load static %}
+{% include "app/ajax/popinfo.js" %}
+
 factionbuildingHeight = 10
+
+faction_control_panel = {top:50,
+    left:300,
+    width:"800px",
+    height:"600px",
+    alpha:0.5,
+    cornerRadius:5,
+    thickness:1,
+    linkOffsetY:30,
+    close_offset_left:465}
+
 
 // light
 const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
@@ -14,16 +27,16 @@ const groundMat = new BABYLON.StandardMaterial("groundMat");
 const center = BABYLON.MeshBuilder.CreateBox("center", {"height":20,"size":1})
     center.isVisible = false
 
-ButtonBox = createRectangle()
+ButtonBox = createRectangle(faction_control_panel)
 ButtonBox.isVisible = false
 
 // pointer 
-const pointer = BABYLON.MeshBuilder.CreateSphere("pointer", {diameter: 20});
+const pointer = BABYLON.MeshBuilder.CreateSphere("pointer", {diameter: 12});
 pointer.position = new BABYLON.Vector3(0,0,0)
 pointer.isVisible = false
 
 function createFaction(n){
-    const box = BABYLON.MeshBuilder.CreateBox(n.objid+"box", 
+    const box = BABYLON.MeshBuilder.CreateBox(n.objid+"_box", 
         {"height":factionbuildingHeight,
         "size":5}
       );
@@ -33,7 +46,7 @@ function createFaction(n){
       boxMat.diffuseTexture =  new BABYLON.Texture("{% static 'app/objects/planet/surface/skyscraper.png' %}");
       box.material = boxMat; 
       
-    var disc = BABYLON.MeshBuilder.CreateCylinder("disc", {diameter:50, height:1});
+    var disc = BABYLON.MeshBuilder.CreateCylinder(n.objid + "disc", {diameter:50, height:1});
         disc.position.y = factionbuildingHeight/2*-1
         disc.parent = box
     const discMat = new BABYLON.StandardMaterial(n.objid + "_groundMat");
@@ -42,13 +55,13 @@ function createFaction(n){
 }
 
 function createPop(n,f){
-    var orbiting = scene.getMeshByName(f.objid+"box");
-    const box = BABYLON.MeshBuilder.CreateBox(n.objid+"box", 
+    var faction = scene.getMeshByName(f.objid+"_box");
+    const box = BABYLON.MeshBuilder.CreateBox(n.objid+"_box", 
         {"height":factionbuildingHeight/2,
         "size":5}
     );
-    box.parent = orbiting
-    box.position = new BABYLON.Vector3(n.coord.x, factionbuildingHeight/4*-1, n.coord.z) 
+    box.parent = faction
+    box.position = new BABYLON.Vector3(n.coord.x + 5, factionbuildingHeight/4*-1, n.coord.z + 5) 
     var phi = Math.random() * 2 * Math.PI; 
     box.rotation.y = phi;
 
@@ -57,27 +70,55 @@ function createPop(n,f){
         box.material = boxMat; 
 }
 
+function getPopBox(f){
+    ButtonBox.dispose()
+    ButtonBox = createRectangle(faction_control_panel)
+    pops = filter_nodes_res(data.nodes,'faction','name', f.data.name)
+    console.log(pops)
+    var guiIter = 0
+    for (let si = 0; si < pops.length; si++) {
+        guiIter++
+        o = {}
+        o.data = pops[si].population
+        console.log(si,"pop", o)
+        o.iter = guiIter
+        o.gui = {buttonColor:"white",
+            depth:1}
+        o.gui.clickButton = function(o) {
+            console.log(o.data.name, o.data.objid, " button was pushed")
+            objectDetails(o.data)
+        };
+        createButton(o)
+        if(o.data.isIdle=="True"){
+            o.gui.buttonColor = "green"
+            o.gui.buttontext = "get actions"
+            actionsButton = createSpecificButton(o, ButtonBox)}
+            actionsButton.onPointerUpObservable.clear()
+            actionsButton.onPointerUpObservable.add(function() {ajax_getActions(o.data)});
+        }
+    ButtonBox.isVisible = true
+}
 
 
+// main - actually loads the content
 var guiIter = 0
 factions = distinct_list(data.nodes,'faction','objid')
 console.log(factions)
 for (let i = 0; i < factions.length; i++) {
     guiIter ++
-    f = get_specific_node(data.nodes,factions[i])[0]   
+    f = {}
+    f.data = get_specific_node(data.nodes,factions[i])[0]   
     f.iter = guiIter
-    pops = filter_nodes_res(data.nodes,'faction','name', f.name)
-    console.log(pops)
+    pops = filter_nodes_res(data.nodes,'faction','name', f.data.name)
     f.coord = pivotLocal(-150,150)
     createFaction(f)
     f.gui = {buttonColor:"white",
             depth:0}
     f.gui.clickButton = function(f) {
-        console.log(f.name, f.objid, " button was pushed", f.coord)
+        console.log(f.data.name, f.data.objid, " button was pushed")
         pointer.position = new BABYLON.Vector3(f.coord.x, 100, f.coord.z) 
         pointer.isVisible = true
-        label = dashboard.getControlByName(n.objid+"_nameplate")
-        if(label){label.isVisible = false}
+        getPopBox(f)
         objectDetails(f)
     };
     createButton(f)
@@ -88,6 +129,8 @@ for (let i = 0; i < factions.length; i++) {
     }
 
   }
+
+
 
 // Goals: 
 // https://doc.babylonjs.com/features/featuresDeepDive/mesh/creation/set/height_map
--- app/templates/app/ui/solar_system.js
+++ app/templates/app/ui/solar_system.js
@@ -25,10 +25,10 @@ function createStar(n){
     n.x = 0
     n.y = 0
     n.z = 0
-    const star = BABYLON.MeshBuilder.CreateSphere(n.objid, {diameter: n.diameter});
+    const star = BABYLON.MeshBuilder.CreateSphere(n.data.objid, {diameter: n.diameter});
     var sunlight = new BABYLON.PointLight("sunlight", new BABYLON.Vector3(n.x, n.y, n.z));
     const surface = new BABYLON.StandardMaterial("surface");
-    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.class]);
+    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.data.class]);
     
     // light coming of sun
     surface.emissiveColor = new BABYLON.Color3(0, 1, 1);
@@ -41,23 +41,23 @@ function createStar(n){
 
 // Planet
 function createPlanet(n){
-    var orbiting = scene.getMeshByName(n.orbitsId);
-    n.diameter = scale_radius(n.radius)/2  // should be r*2 but I want smaller plantets. 
-    n.x = scale_distance(guiIter)
+    var orbiting = scene.getMeshByName(n.data.orbitsId);
+    n.diameter = scale_radius(n.data.radius)/2  // should be r*2 but I want smaller plantets. 
+    n.x = scale_distance(n.iter)
     n.y = 0
-    n.z = scale_distance(n.orbitsDistance)
-    const planet = BABYLON.MeshBuilder.CreateSphere(n.objid,  {diameter: n.diameter});
+    n.z = scale_distance(n.data.orbitsDistance)
+    const planet = BABYLON.MeshBuilder.CreateSphere(n.data.objid,  {diameter: n.diameter});
     planet.parent = orbiting
     planet.position = new BABYLON.Vector3(n.x, n.y, n.z);
     var phi = Math.random() * 2 * Math.PI; 
     planet.rotation.y = phi;
     // texture
     const surface = new BABYLON.StandardMaterial("surface");
-    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.class]);
+    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.data.class]);
     planet.material = surface
     planet.material.specularColor = new BABYLON.Color3(shinyness, shinyness, shinyness);
 
-    var rect1 = new BABYLON.GUI.Rectangle(n.objid+"_nameplate");
+    var rect1 = new BABYLON.GUI.Rectangle(n.data.objid+"_nameplate");
         rect1.width = .06;
         rect1.height = .03;
         rect1.cornerRadius = 10;
@@ -69,32 +69,32 @@ function createPlanet(n){
         rect1.linkOffsetY = -15;
 
 
-    var label = new BABYLON.GUI.TextBlock(n.objid+"_nameplatetext");
+    var label = new BABYLON.GUI.TextBlock(n.data.objid+"_nameplatetext");
         // https://playground.babylonjs.com/#XCPP9Y#121
-        label.text = n.name;
+        label.text = n.data.name;
         label.height = "40px"
-        rect1.addControl(label);   
+        rect1.addControl(label);    
 }
 
 // Moon
 function createMoon(n){
-    var orbiting = scene.getMeshByName(n.orbitsId);
-    var o_n = get_node(solar_system["nodes"], n.orbitsId)
-    n.diameter = scale_radius(n.radius)/2  // should be r*2 but I want smaller plantets. 
-    n.x = (scale_distance_ln(n.orbitsDistance) + 20) * flipper()
-    n.y = (scale_distance_ln(n.orbitsDistance) + 20) * flipper()
-    n.z = (scale_distance_ln(n.orbitsDistance) + 20) * flipper()
-    if(n.orbitsName=="Earth"){console.log("orbiting", o_n)}
-    const moon = BABYLON.MeshBuilder.CreateSphere(n.objid,  {diameter: n.diameter});
+    var orbiting = scene.getMeshByName(n.data.orbitsId);
+
+    n.diameter = scale_radius(n.data.radius)/2  // should be r*2 but I want smaller plantets. 
+    n.x = (scale_distance_ln(n.data.orbitsDistance) + 20) * flipper()
+    n.y = (scale_distance_ln(n.data.orbitsDistance) + 20) * flipper()
+    n.z = (scale_distance_ln(n.data.orbitsDistance) + 20) * flipper()
+
+    const moon = BABYLON.MeshBuilder.CreateSphere(n.data.objid,  {diameter: n.diameter});
     moon.parent = orbiting
     moon.position = new BABYLON.Vector3(n.x, n.y, n.z);
     var phi = Math.random() * 2 * Math.PI; 
     moon.rotation.y = phi;
     // texture
     const surface = new BABYLON.StandardMaterial("surface");
-    surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.class]);
-    moon.material = surface
-    moon.material.specularColor = new BABYLON.Color3(shinyness, shinyness, shinyness);
+        surface.diffuseTexture =  new BABYLON.Texture(sphere_textures[n.data.class]);
+        moon.material = surface
+        moon.material.specularColor = new BABYLON.Color3(shinyness, shinyness, shinyness);
 }
 
 
@@ -102,17 +102,18 @@ function createMoon(n){
 // Primary objects that don't rely on the relative position
 var guiIter = 0
 for (let i = 0; i < solar_system.nodes.length; i++) {
-    n = solar_system["nodes"][i]
+    n = {}
+    n.data = solar_system["nodes"][i]
     n.gui = {buttonColor:"white",
             depth:0}
-    if (n["objtype"]=="star"){
+    if (n.data.objtype=="star"){
         n.gui.clickButton = function(n) {
-            console.log(n.name, n.objid, " button was pushed")
+            console.log(n.data.name, n.data.objid, " button was pushed")
             camera.setTarget(new BABYLON.Vector3(n.x, n.y, n.z));
             camera.radius = n.diameter + 25  
-            label = dashboard.getControlByName(n.objid+"_nameplate")
+            label = dashboard.getControlByName(n.data.objid+"_nameplate")
             if(label){label.isVisible = false}
-            objectDetails(n)
+            objectDetails(n.data)
             ButtonBox.dispose()
         }
         guiIter++
@@ -120,48 +121,48 @@ for (let i = 0; i < solar_system.nodes.length; i++) {
         createStar(n)
         createButton(n)
     }
-    if (n["objtype"]=="planet"){
+    if (n.data.objtype=="planet"){
         guiIter++
         n.iter = guiIter
         n.gui.clickButton = function(n) {
-            console.log(n.name, n.objid, " button was pushed")
+            console.log(n.data.name, n.data.objid, " button was pushed")
             camera.setTarget(new BABYLON.Vector3(n.x, n.y, n.z));
             camera.radius = n.diameter + 25  
-            label = dashboard.getControlByName(n.objid+"_nameplate")
+            label = dashboard.getControlByName(n.data.objid+"_nameplate")
             if(label){label.isVisible = false}
-            objectDetails(n)
+            objectDetails(n.data)
             ButtonBox.dispose()
-            ButtonBox = createRectangle()
+            ButtonBox = createRectangle(generic_control_panel)
 
-            satellites = scene.getMeshByName(n.objid).getChildren()
+            satellites = scene.getMeshByName(n.data.objid).getChildren()
             // console.log(satellites)
             var guiIter = 0
             for (let si = 0; si < satellites.length; si++) {
                 guiIter++
-                o = get_node(solar_system["nodes"],satellites[si].id)
+                o = {}
+                o.data = get_node(solar_system["nodes"],satellites[si].id)
+                console.log(o)
                 o.iter = guiIter
                 o.gui = {buttonColor:"white",
                     depth:1}
                 o.gui.clickButton = function(o) {
-                    console.log(o.name, o.objid, " button was pushed")
-                    objectDetails(o)
+                    console.log(o.data.name, o.data.objid, " button was pushed")
+                    objectDetails(o.data)
                 };
                 if(o.isSupportsLife=="True"){
-                    console.log(o.name,o.isPopulated)
                     o.gui.buttonColor = "green"
                 }
                 createButton(o)
-                if(o.isSupportsLife=="True"){createVisitButton(o)}
+                if(o.data.isSupportsLife=="True"){createVisitButton(o)}
             }
         }
         createPlanet(n)
         
-        if(n.isSupportsLife=="True"){
-            console.log(n.name,n.isPopulated)
+        if(n.data.isSupportsLife=="True"){
             n.gui.buttonColor = "green"
         }
         createButton(n)
-        if(n.isSupportsLife=="True"){createVisitButton(n)}
+        if(n.data.isSupportsLife=="True"){createVisitButton(n)}
     }
   }
 
@@ -169,10 +170,11 @@ for (let i = 0; i < solar_system.nodes.length; i++) {
 // Secondary objects that do rely on a position of something else.
 var guiIter = 0
 for (let i = 0; i < solar_system.nodes.length; i++) {
-    n = solar_system["nodes"][i]
+    n = {}
+    n.data = solar_system["nodes"][i]
     guiIter++
     n.iter = guiIter
-    if (n["objtype"]=="moon"){
+    if (n.data.objtype=="moon"){
         createMoon(n)
     }
 }
