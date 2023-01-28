7b26f4ff34216d11562f2d3984f4afb7bf7bd9db
billmanh <william.jeffrey.harding@gmail.com>
2023-01-27 18:19:21 -0800

    fixed the logic in the actions

--- functions/getActions/__init__.py
+++ functions/getActions/__init__.py
@@ -1,34 +1,55 @@
 import logging
 from gremlin_python.driver import client, protocol, serializer
 from gremlin_python.driver.protocol import GremlinServerError
-import os, yaml
+import os, yaml, json
 
 import azure.functions as func
 from .cmdb_graph import CosmosdbClient
 from .validators import ActionValidator
 
-
-if 'actions.yaml' in os.listdir():
-    actions = yaml.safe_load('./actions.py')
-else: 
-    actions = yaml.safe_load('getActions/actions.py')
+def get_actions():
+    if 'actions.yaml' in os.listdir():
+        actions = yaml.safe_load(open('actions.yaml'))
+    else: 
+        actions = yaml.safe_load(open('./getActions/actions.yaml'))
+    return actions["actions"]
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    c = CosmosdbClient()
     logging.info('************* Python HTTP trigger function processed a request.')
+    
+    c = CosmosdbClient()
     logging.info(f'CDB endpoint: {c.endpoint}')
     
+    actions = get_actions()
+    logging.info(f'total actions available: {len(actions)}')
+    logging.info(f'total actions available: {[a["applies_to"] for a in actions]}')
 
-    req_body = req.get_json()
-    request = req_body.get('agent')
+    agent_id = req.params.get('objid')
     
 
-    if request:
-        logging.info('************* Python HTTP trigger function processed a request.')
+    if agent_id:
+        logging.info(f'************* Python HTTP trigger function processed a request. for {agent_id}')
         resp_json = {}
-        c.run_query(f"g.V().has('objid','{request.get('objid')}')")
-        agent = c.clean_node(c.res)
+        c.run_query(f"g.V().has('objid','{agent_id}').valueMap()")
+        agent = c.clean_nodes(c.res)
+        logging.info(f'{agent_id} returns agents: {agent}')
+
+        if len(agent)==0:
+            logging.info(f'{agent_id} returns blank agent: {agent}')
+            return func.HttpResponse(
+                "error: agent not found",
+                status_code=200
+            )
+        if len(agent)>1:
+            logging.info(f'{agent_id} returns multiple agents: {agent}')
+            return func.HttpResponse(
+                "error: duplicate agents",
+                status_code=200
+            )
+
+        agent = agent[0]
+        logging.info(f'{agent_id} returns agent named: {agent.get("name")}')
         validator = ActionValidator(agent,actions)
         valid_actions = validator.validate()
         if len(valid_actions)>0:
@@ -36,14 +57,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             resp_json['error'] = "no actions returned"
             
-        func.HttpResponse(resp_json)
             
 
-        return func.HttpResponse(resp_json)
+        return func.HttpResponse(json.dumps(resp_json),status_code=200)
     else:
-        logging.info(f'req_body is bad: {req_body}')
+        logging.info(f'req_body is bad: {agent_id}')
         return func.HttpResponse(
-             "error: action requires agent"
+             "error: action requires agent",
              status_code=200
         )
     
