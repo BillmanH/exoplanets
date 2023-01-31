cc73c0757abf38e4ea0652ec57ef7ac3c95cce53
billmanh <william.jeffrey.harding@gmail.com>
2023-01-31 12:09:07 -0800

    Rewrote time function for new action resolution process

--- functions/time/__init__.py
+++ functions/time/__init__.py
@@ -1,16 +1,23 @@
 import datetime
 import logging
+import pandas as pd
 
 import azure.functions as func
-from .db_functions import get_client, run_query, clean_node
+from .cmdb_graph import CosmosdbClient
+
 from .time_functions import global_ticker
-from .action import get_global_actions, validate_action, augments_self_properties
+from .action import (get_global_actions, 
+                        validate_action_time, 
+                        parse_properties, 
+                        resolve_action,
+                        mark_action_as_resolved,
+                        mark_agent_idle)
 
 logger = logging.getLogger('azure.mgmt.resource')
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    c = get_client()
+    c = CosmosdbClient()
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
@@ -18,37 +25,31 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
     
     # Time is constant, pulled in once at the begining of the function
-    res = run_query(c,"g.V().hasLabel('time').valueMap()")
-    time = [clean_node(n) for n in res][0]
+    c.run_query(c,"g.V().hasLabel('time').valueMap()")
+    time = c.clean_nodes(c.res)[0]
 
     ### START of time_funtions here
 
     # Get all pending actions
     actions = get_global_actions(c)
+    actions_df = pd.DataFrame(actions)
 
-    # resolve each actionn 
     validActionCounter = 0
-    for a in actions:
-        if validate_action(time,a['job']):
+    for i in actions_df.index:
+        agent = parse_properties(actions_df.loc[i].agent)
+        action = parse_properties(actions_df.loc[i].action)
+        job = parse_properties(actions_df.loc[i].job)
+
+        if validate_action_time(time,job):
             validActionCounter += 1
-            if 'augments_self_properties' in list(a['job'].keys()):
-                logging.info(f"resolving augments_self_properties for: {a['agent']}")
-                patch_query = augments_self_properties(a['agent'],a['job'])
-                patch = run_query(c, patch_query)
-            # set the job to resolved
-            patch_job = f"""
-            g.V().has('objid','{a['agent']['objid']}')
-                    .outE('takingAction')
-                    .has('actionType', '{a['job']['actionType']}')
-                    .has('weight','{a['job']['weight']}')
-                    .property('status', 'resolved')
-            """
-            logging.info(f'updating job: {patch_job}')
-            res = run_query(c, patch_job)
-            # set the agent to isIdle=True
-            if 'isIdle' in list(a['agent'].keys()):
-                res = run_query(c, f"g.V().has('objid','{a['agent']['objid']}').property('isIdle','True')")
-                logging.info(f"agent set to idle: {a['agent']['objid']}")
+            resolve_action(c,agent,action)
+            mark_action_as_resolved(c,agent, job)
+            mark_agent_idle(c, agent)
+
+            c.run_queries()
+
+            logging.info(f'validActionCounter: {validActionCounter}')
+            
 
     logging.info(f'Total ations resolved in this run: {validActionCounter}')
     # Increment global time
