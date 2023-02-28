a7e4e45174298129a6dfdfb8450a883ce9d12c0e
billmanh <william.jeffrey.harding@gmail.com>
2023-02-28 12:11:40 -0800

    resources now have a replenish rate [optional]

--- app/creators/universe.py
+++ app/creators/universe.py
@@ -20,14 +20,20 @@ class Resource:
         self.volume = maths.rnd(conf["mean"],conf["std"], min_val=0)
         if conf.get('replenish_rate'):
             self.replenish_rate = conf['replenish_rate']
+        else: 
+            self.replenish_rate = None
     def get_data(self):
-        return {
+        fund = {
             "name": self.name,
             "objid": self.objid,
             "label": self.label,
             "volume": self.volume,
             "description": self.description
         }
+        if (self.replenish_rate):
+            fund['replenish_rate'] = self.replenish_rate
+        return fund
+
     def __repr__(self) -> str:
         return f"<{self.label}: {self.objid}; {self.name}>"  
 
