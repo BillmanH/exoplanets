b9e9e35ed816bfa252d06a7974d2d80e9572ace8
billmanh <william.jeffrey.harding@gmail.com>
2023-06-27 09:16:40 -0700

    placehoder for simulation process

---
+++ simulation/run_sim.py
@@ -0,0 +1,12 @@
+# %%
+import sim
+import sys
+
+# %%
+# mapping to the modules that make the app
+sys.path.insert(0, "..")
+
+from app.creators import homeworld
+from app.creators import universe
+# %%
+
