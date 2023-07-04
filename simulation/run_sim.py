7d9238845524c25356729a117844756646756990
billmanh <william.jeffrey.harding@gmail.com>
2023-07-04 12:18:04 -0700

    sim to generate excel dataset

--- simulation/run_sim.py
+++ simulation/run_sim.py
@@ -1,8 +1,9 @@
 # %%
-import sim
+# import sim
 import sys
 import yaml
 import pandas as pd
+
 
 # %%
 # mapping to the modules that make the app
@@ -10,6 +11,8 @@ sys.path.insert(0, "..")
 
 from app.creators import homeworld
 from app.creators import universe
+from app.functions import maths
+
 # %%
 
 
@@ -25,7 +28,21 @@ class Simulaiton:
         self.systems = []
 
     def break_down_datasets(self):
-        nodes = pd.concat([pd.DataFrame(i['nodes']) for i in sim.systems])
+        nodes = pd.concat([pd.DataFrame(i['nodes']) for i in self.systems])
+        writer = pd.ExcelWriter('simulation_output.xlsx')
+        
+        for i in nodes['label'].unique():
+            df = nodes[nodes['label']==i]
+            df = df.loc[:,df.notna().any(axis=0)]
+            df.to_excel(writer, sheet_name=f'{i}_nodes')
+
+        edges = pd.concat([pd.DataFrame(i['edges']) for i in self.systems])
+        edges.to_excel(writer, sheet_name=f'edges')
+        writer.close()
+
+
+
+
 
 # %%
 def main():
@@ -34,12 +51,12 @@ def main():
     print(f"running a simulation on {config.config['simulation']['n_players']} players")
     for iter,item in enumerate(config.sims):
         print(f"generating sim: {iter}")
+        # to give forms a random user ID.
+        config.user_input['objid'] = maths.uuid()
+        config.user_input['accountid'] = maths.uuid()
         sim.systems.append(universe.build_homeSystem(config.user_input, username="simulation"))
 
-    nodes = pd.concat([pd.DataFrame(i['nodes']) for i in sim.systems])
-    nodes.reset_index(drop=False).to_csv('simulation_nodes_out.csv')
-    edges = pd.concat([pd.DataFrame(i['edges']) for i in sim.systems])
-    edges.reset_index(drop=False).to_csv('simulation_edges_out.csv')
+    sim.break_down_datasets()
 
 if __name__ == "__main__":
     main()
