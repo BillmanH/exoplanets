5cd184fcd668403731a659882e69286db246efc4
billmanh <william.jeffrey.harding@gmail.com>
2023-08-01 19:47:33 -0700

    Beginning Testing on Pop Growth

--- functions/popgrowth/__init__.py
+++ functions/popgrowth/__init__.py
@@ -1,13 +1,13 @@
 import datetime
 import logging
 import yaml, pickle
-import pandas as pd
-import numpy as np
 
 import azure.functions as func
-from .cmdb_graph import CosmosdbClient
+
+
 from .tools import consumption, growth
-import os
+from .app.connectors import cmdb_graph
+from .app.objects import time
 
 logger = logging.getLogger('azure.mgmt.resource')
 
@@ -23,7 +23,7 @@ except:
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    c = CosmosdbClient()
+    c = cmdb_graph.CosmosdbClient()
     logging.info(f'CDB endpoint: {c.endpoint}')
     logging.info(f"[param] amount people will suffer by starving: {params['starve_damage']}")
     logging.info(f"[param] amount of health needed to grow: {params['pop_health_requirement']}")
@@ -33,10 +33,14 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
     
-    c.run_query("g.V().hasLabel('time').valueMap()")
-    params['time'] = c.clean_nodes(c.res)[0]
+
+    t = time.Time(c)
+    t.get_current_UTU()
+    params['currentTime'] = t.params['currentTime']
+
 
     consumption.consume(c,params)
+    
     growth.grow(c,params,syllables)
 
     ### END 
