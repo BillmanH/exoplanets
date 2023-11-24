a32f38a989a566b1f3853f4f32d5c12b70fee9e6
billmanh <william.jeffrey.harding@gmail.com>
2023-11-24 14:48:22 -0800

    .

--- az-functions/function_app.py
+++ az-functions/function_app.py
@@ -7,8 +7,15 @@ import logging
 app = func.FunctionApp()
 
 
-@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name=os.getenv("EVENT_HUB_NAME"),
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="EXO_TIME",
                                connection="EVENT_HUB_CONNECTION_STR")
-def myFunction(azeventhub: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
+def eh_update_time(azeventhub: func.EventHubEvent):
+    logging.info('Time Event Was triggered: %s',
+                azeventhub.get_body().decode('utf-8'))
+
+
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="EXO_ACTION",
+                               connection="EVENT_HUB_CONNECTION_STR")
+def eh_resolve_action(azeventhub: func.EventHubEvent):
+    logging.info('Action Event was triggered: %s',
                 azeventhub.get_body().decode('utf-8'))
