41ae5f166921c5985a95f2c9770056b1b520e508
billmanh <william.jeffrey.harding@gmail.com>
2024-02-09 13:33:33 -0800

    Confirmed new functions are working.

---
+++ func/components/actions.py
@@ -0,0 +1,110 @@
+import datetime
+import pandas as pd
+import numpy as np
+import yaml
+
+
+
+class Action:
+    """
+    All Actions must have an `Agent` who does the action, 
+        an `Action` the thing that is done.
+        and a `Job` that is the edge between the two in the graph
+    """
+    def __init__(self,c,action):
+        self.agent = action['agent']
+        self.action = action['action']
+        self.job = action['job']
+        self.c = c
+        self.data = {"nodes":[],"edges":[]}
+
+    # TODO: Inherit from a base class
+    def uuid(self, n=13):
+        return "".join([str(i) for i in np.random.choice(range(10), n)])
+
+    def validate_action_time(self,time):
+        '''
+        validate that the time to complete the task has passed. 
+        Not the same as validation that the action can be taken. 
+        If the `takingAction` edge has been applied to the agent `a`, then it is just a check of time completed. 
+        '''
+        if int(self.job['weight']) < int(time.params['currentTime']):
+            return True
+        else:
+            return False
+
+    def resolve_augments_self_properties(self):
+        agent = self.agent.copy()
+        self_properties = yaml.safe_load(self.action["augments_self_properties"])
+        for p in self_properties.keys():
+            agent[p] = agent[p] + float(self_properties[p])
+        return agent
+
+    def mark_action_as_resolved(self):
+        patch_job = f"""
+        g.V().has('objid','{self.agent['objid']}')
+                .outE('takingAction')
+                .has('actionType', '{self.job['actionType']}')
+                .has('weight','{self.job['weight']}')
+                .property('status', 'resolved')
+        """    
+        self.c.add_query(patch_job.replace(" ", "").replace("\n", ""))
+
+    def mark_agent_idle(self):
+        if 'isIdle' in self.agent.keys():
+            idleQuery = f"""
+            g.V().has('objid','{self.agent['objid']}').property('isIdle','true')
+            """
+            self.c.add_query(idleQuery.replace(" ", "").replace("\n", ""))
+        
+    def make_action_event(self,time):
+        """
+        Creates both the node and the edge for the action
+        """
+        node = {
+            'objid': f"{self.uuid()}",
+            'name':'job',
+            'label':'event',
+            'text': f"The {self.agent['objtype']} ({self.agent['name']}) has completed {self.job['actionType']}",
+            'visibleTo':self.agent['userguid'],
+            'time': time.params['currentTime'],
+            'username':'event'
+        }
+        self.data['nodes'].append(node)
+        self.data['edges'].append({'node1':self.agent['objid'],'node2':node['objid'],'label':'completed'})
+        
+
+    def query_patch_properties(self):
+        query = f"g.V().has('objid','{self.agent['objid']}')"
+        for n in yaml.safe_load(self.action["augments_self_properties"]):
+            query += f".property('{n}',{self.agent[n]})"
+        self.c.add_query(query.replace(" ", "").replace("\n", ""))
+
+    def add_updates_to_c(self,time):
+        if self.action.get('augments_self_properties') != None:
+            self.query_patch_properties()
+        self.make_action_event(time)
+        self.mark_action_as_resolved()
+        self.mark_agent_idle()
+
+    def make_building(self):
+        pass
+
+
+    def resolve_action(self):
+        if len(self.c.stack)>0:
+            self.c.run_queries()
+        
+        self.c.upload_data(self.agent['userguid'],self.data)
+
+    def get_action_message(self):
+        message = {"agent":self.agent,"action":self.action,"job":self.job}
+        return message
+
+    def __repr__(self) -> str:
+        if self.action.get('name') != None:
+            name = self.action.get('name')
+        else:
+            name = self.action.get('type')
+        return f"< ({self.agent.get('name')}: {self.agent.get('objid')}) -{self.job.get('name')}:{self.job.get('weight')}-> ({name}) >"
+    
--- func/function_app.py
+++ func/function_app.py
@@ -1,20 +1,32 @@
 import azure.functions as func
 import datetime
 import json
+import ast
 import os
 import logging
 
+from components import actions
+from components import cmdb_graph
+from components import time
 app = func.FunctionApp()
+
 
 EVENT_HUB_FULLY_QUALIFIED_NAMESPACE = os.environ.get('EVENT_HUB_FULLY_QUALIFIED_NAMESPACE')
 EVENT_HUB_CONNECTION_STR = os.environ.get('EVENT_HUB_CONNECTION_STR')
 EVENT_HUB_NAME = os.environ.get('EVENT_HUB_NAME')
 
-@app.function_name(name="EventHubTrigger1")
+@app.function_name(name="resolveActionEvents")
 @app.event_hub_message_trigger(arg_name="event",
                                event_hub_name=EVENT_HUB_NAME,
                                connection="EVENT_HUB_CONNECTION_STR")
 def eventGridTest(event: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
-                event.get_body().decode('utf-8'))
+    message = ast.literal_eval(event.get_body().decode('utf-8'))
+    logging.info(f'Python EventHub trigger processed an messasge: {message} : {type(message)}')
+    c = cmdb_graph.CosmosdbClient()
+    t = time.Time(c)
+    t.get_current_UTU()
+    action = actions.Action(c,message)
+    action.add_updates_to_c(t)
+    c.upload_data(action.agent['userguid'], action.data)
+    logging.info(f'      -------And with that processed an action: {action} at UTU:{t}')
 
