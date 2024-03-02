9af0eac97b1650055931ea314f015d5dce13732c
billmanh <william.jeffrey.harding@gmail.com>
2024-03-02 08:44:50 -0800

    updated common function libraries

--- function_app.py
+++ function_app.py
@@ -8,12 +8,11 @@ import ast
 import os
 import logging
 
-from components import actions
-from components import cmdb_graph
-from components import time
-from components import jobs
+from app.objects import time
+from app.connectors import cmdb_graph
+from app.functions import jobs
 
-app = func.FunctionApp()
+azapp = func.FunctionApp()
 
 
 EVENT_HUB_FULLY_QUALIFIED_NAMESPACE = os.environ.get('EVENT_HUB_FULLY_QUALIFIED_NAMESPACE')
@@ -26,8 +25,8 @@ credential = DefaultAzureCredential()
 # func start --functions [a space separated list of functions]
 # func start --functions actionResolverTimer resolveActionEvents ututimer
 
-@app.function_name(name="resolveActionEvents")
-@app.event_hub_message_trigger(arg_name="event",
+@azapp.function_name(name="resolveActionEvents")
+@azapp.event_hub_message_trigger(arg_name="event",
                                event_hub_name=EVENT_HUB_NAME,
                                connection="EVENT_HUB_CONNECTION_STR")
 def eventGridTest(event: func.EventHubEvent):
@@ -36,14 +35,14 @@ def eventGridTest(event: func.EventHubEvent):
     c = cmdb_graph.CosmosdbClient()
     t = time.Time(c)
     t.get_current_UTU()
-    action = actions.Action(c,message)
+    action = time.Action(c,message)
     action.add_updates_to_c(t)
     c.upload_data(action.agent['userguid'], action.data)
     logging.info(f'      -------And with that processed an action: {action} at UTU:{t}')
 
 # Check the open actions and resolve them
-@app.function_name(name="actionResolverTimer")
-@app.schedule(schedule="0 */2 * * * *", 
+@azapp.function_name(name="actionResolverTimer")
+@azapp.schedule(schedule="0 */2 * * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
@@ -54,13 +53,13 @@ def test_function(mytimer: func.TimerRequest) -> None:
     c = cmdb_graph.CosmosdbClient()
     t = time.Time(c)
     t.get_current_UTU()
-    messages = jobs.resolve_jobs(c,t,actions)
+    messages = jobs.resolve_jobs(c,t,time.Action)
     jobs.send_to_eventhub(messages, eh_producer)
     logging.info(f'Total Messages sent to EH: {len(messages)} at: {utc_timestamp}')
 
 # UTU is the universal time unit
-@app.function_name(name="ututimer")
-@app.schedule(schedule="0 */2 * * * *", 
+@azapp.function_name(name="ututimer")
+@azapp .schedule(schedule="0 */2 * * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
