68464936ce2aa24e9e823e30f39002adcdbd1dec
billmanh <william.jeffrey.harding@gmail.com>
2024-03-02 21:30:55 -0800

    Trying to see if the functions wll run if they are in the root.

--- function_app.py
+++ function_app.py
@@ -8,11 +8,11 @@ import ast
 import os
 import logging
 
-from app.objects import time
-from app.connectors import cmdb_graph
-from app.functions import jobs
+import az_func_time as time
+import cmdb_graph
+import jobs
 
-azapp = func.FunctionApp()
+app = func.FunctionApp()
 
 
 EVENT_HUB_FULLY_QUALIFIED_NAMESPACE = os.environ.get('EVENT_HUB_FULLY_QUALIFIED_NAMESPACE')
@@ -25,8 +25,8 @@ credential = DefaultAzureCredential()
 # func start --functions [a space separated list of functions]
 # func start --functions actionResolverTimer resolveActionEvents ututimer
 
-@azapp.function_name(name="resolveActionEvents")
-@azapp.event_hub_message_trigger(arg_name="event",
+@app.function_name(name="resolveActionEvents")
+@app.event_hub_message_trigger(arg_name="event",
                                event_hub_name=EVENT_HUB_NAME,
                                connection="EVENT_HUB_CONNECTION_STR")
 def eventGridTest(event: func.EventHubEvent):
@@ -41,8 +41,8 @@ def eventGridTest(event: func.EventHubEvent):
     logging.info(f'      -------And with that processed an action: {action} at UTU:{t}')
 
 # Check the open actions and resolve them
-@azapp.function_name(name="actionResolverTimer")
-@azapp.schedule(schedule="0 */2 * * * *", 
+@app.function_name(name="actionResolverTimer")
+@app.schedule(schedule="0 */2 * * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
@@ -58,8 +58,8 @@ def test_function(mytimer: func.TimerRequest) -> None:
     logging.info(f'Total Messages sent to EH: {len(messages)} at: {utc_timestamp}')
 
 # UTU is the universal time unit
-@azapp.function_name(name="ututimer")
-@azapp .schedule(schedule="0 */2 * * * *", 
+@app.function_name(name="ututimer")
+@app .schedule(schedule="0 */2 * * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
