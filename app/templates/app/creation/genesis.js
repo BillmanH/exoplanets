6b4c5ea5ab63cbab575dbe7a26a86721a88c91c2
billmanh <william.jeffrey.harding@gmail.com>
2024-08-11 16:36:24 -0700

    added images to genesis

--- app/templates/app/creation/genesis.js
+++ app/templates/app/creation/genesis.js
@@ -1,5 +1,6 @@
 
 // NOTE: These just affect the UI in the creator. This does not affect the actual genesis limits.
+{% load static %}
 
 var form = {
     "label":"form",
@@ -82,14 +83,21 @@ function add_duality_slider(c,n1,n2){
     slider.oninput = function() {
         form[n1] = (this.value/100).toFixed(3);
         form[n2] = (1-(this.value/100)).toFixed(3);
-        document.getElementById(n1+"_text").innerText = n1 + ": "+ form[n1] 
-        document.getElementById(n2+"_text").innerText = n2 + ": "+ form[n2]
+        document.getElementById(n1+"_text").innerText = n1 + ": "+ (form[n1] * 100).toFixed(0) + "%"
+        document.getElementById(n2+"_text").innerText = n2 + ": "+ (form[n2] * 100).toFixed(0) + "%"
       }
     document.getElementById(c).appendChild(div);  
 }
 
 
-
+function add_image(elem,link){
+    var img = document.createElement("img")
+    img.src = link
+    img.style.height = 150;
+    img.style.width = 150;
+    img.style.padding = 10;
+    document.getElementById(elem).appendChild(img)
+}
 
 function add_p(i,n) {
     var p = document.createElement("p")       
@@ -105,11 +113,11 @@ add_p("accountinfo","created: " + account.created)
 add_p("accountinfo","last used: "+account.last_used)
 add_p("accountinfo","Owner: "+account.username)
 
-function back_to_home(c,n){
+function back_to_home(){
     window.location.href = '/';
 }
 
-function delete_existing_game(c,n) {
+function delete_existing_game() {
     $.ajax({
         url: '/ajax/refreshaccount',
         type: 'get',
@@ -130,7 +138,7 @@ function delete_existing_game(c,n) {
     });
 }
 
-function build_solar_system(c,n) {
+function build_solar_system() {
     $.ajax({
         url: '/ajax/genesissystem',
         type: 'get',
@@ -150,33 +158,7 @@ function build_solar_system(c,n) {
     });
 }
 
-if(account.type=="pre_beta_account"){
-    add_p("accountinfo","your account requires that you can only have one account open at a time. Do you want to delete your old game and start over?")
-    add_button("accountinfo",1,"delete everything, start over", delete_existing_game)
-    add_button("accountinfo",2,"return to the main menu", back_to_home)
-}
-
-function form_solar_system(){
-    // TODO: Create text field to allow user to name planet (`planet_name`)
-    add_menu("Choose the nature of your homeworld." , "starting_homeworld")
-    add_p("starting_homeworld","Organic materials will sustain your population in the beginning, minerals will help your population later.")
-    add_duality_slider("starting_homeworld","organics","minerals")
-    add_button("starting_homeworld",1,"Build solar system, and homeworld", build_solar_system)
-}
-
-function form_population(){
-    add_menu("Against all odds, a species on this planet has risen out of the muck and begun to form a culture.", "starting_pop")
-}
-
-function form_people_culture(){
-    add_menu("That species has established a culture that enables them to thrive.", "home_population")
-    add_p("home_population","Choose the starting attributes of your people. Each attribute has both advantages and disadvantages.")
-    add_duality_slider("home_population","conformity","constitution")
-    add_duality_slider("home_population","literacy","aggression")
-    add_button("home_population",1,"Let the people evolve.", build_population)
-}
-
-function build_population(c,n) {
+function build_population() {
     $.ajax({
         url: '/ajax/genesispopulation',
         type: 'get',
@@ -190,18 +172,9 @@ function build_population(c,n) {
         success: function (data) {
             console.log("pops, were created")
             if ("pops" in data) {
-                draw_table(
-                    "peopleTable",
-                    data['pops'],
-                    pop_table_lables
-                )
                 console.log(data['factions'])
                 console.log('homeworld id:', data.homeworldid)
-                draw_table(
-                    "FactionTable",
-                    data['factions'],
-                    faction_table_lables
-                )
+
             }
             finalize_world(data)
             d3.selectAll('#loading').remove()
@@ -210,8 +183,44 @@ function build_population(c,n) {
 }
 
 function goto_system(data){
-            window.location.href = '/popuilocal?objid='+ data.homeworldid;
+    window.location.href = '/popuilocal?objid='+ data.homeworldid;
 }
+
+
+// Step 1: verify the account and request the user to delete the existing game
+if(account.type=="pre_beta_account"){
+    add_p("accountinfo","your account requires that you can only have one account open at a time. Do you want to delete your old game and start over?")
+    add_button("accountinfo",1,"delete everything, start over", delete_existing_game)
+    add_button("accountinfo",2,"return to the main menu", back_to_home)
+}
+
+// Step 2: Build the solar system, after success of the delete_existing_game
+function form_solar_system(){
+    add_menu("Choose the nature of your homeworld." , "starting_homeworld")
+    add_p("starting_homeworld","Organic materials will sustain your population in the beginning, minerals will help your population later.")
+    add_image("starting_homeworld","{% static 'app/objects/icons/img/RockyPlanet.png' %}")
+    add_image("starting_homeworld","{% static 'app/objects/icons/img/GreenPlanet.png' %}")
+    add_duality_slider("starting_homeworld","organics","minerals")
+    add_button("starting_homeworld",1,"Build solar system, and homeworld", build_solar_system)
+}
+
+function form_population(){
+    add_menu("Against all odds, a species on this planet has risen out of the muck and begun to form a culture.", "starting_pop")
+}
+
+// Step 3: Build the population, after success of the build_solar_system
+function form_people_culture(){
+    add_menu("That species has established a culture that enables them to thrive.", "home_population")
+    add_p("home_population","Choose the starting attributes of your people. Each attribute has both advantages and disadvantages.")
+    add_image("home_population","{% static 'app/objects/icons/img/Pop_Constitution.png' %}")
+    add_image("home_population","{% static 'app/objects/icons/img/Pop_Conformist.png' %}")
+    add_duality_slider("home_population","conformity","constitution")
+    add_image("home_population","{% static 'app/objects/icons/img/Pop_agressive.png' %}")
+    add_image("home_population","{% static 'app/objects/icons/img/Pop_literate.png' %}")
+    add_duality_slider("home_population","literacy","aggression")
+    add_button("home_population",1,"Let the people evolve.", build_population)
+}
+
 
 
 function finalize_world(data){
